229092624de0160f0946b4491a1b8e48368f7612
Anuneet Rastogi <128411242+Anuneet2003@users.noreply.github.com>
2025-07-11 13:06:14 +0530

    Update Q6.java

--- Week1/Assignment2/Core Java/Q6.java
+++ Week1/Assignment2/Core Java/Q6.java
@@ -13,7 +13,22 @@ public class Employee {
     private String name;
     private String department;
 
-    // Constructors, Getters, Setters
+    // Constructor
+    public Employee(int id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    // Getters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getDepartment() { return department; }
+
+    // Setters
+    public void setName(String name) { this.name = name; }
+    public void setDepartment(String department) { this.department = department; }
+
 }
 
 // EmployeeCRUD class:
